1c4744ca7ca032a9be8c781a399399f7087abd0b
Ana <aparakaki17@gmail.com>
2018-10-04 23:16:55 -0700

    add input boxes

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 var keys = require("./keys.js");
 accountSid = keys.accountSid;
-authToken = keys.authToken; 
-const client = require('twilio')(accountSid, authToken); 
- 
-client.messages 
-      .create({ 
-         body: 'Hello', 
-         from: '+16266584299',       
-         to: keys.phone
-       }) 
-      .then(function(response) {
+authToken = keys.authToken;
+const client = require('twilio')(accountSid, authToken);
 
-      }) 
-      .done();
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/sendText", function (req, res) {
+console.log(req.body)
+var number =  req.body.phone.replace(/\D/g,'');
+console.log(number);
+  client.messages
+    .create({
+      body: req.body.msg,
+      from: '+16266584299',
+      to: number
+    })
+    .then(function (response) {
+
+    })
+    .done();
+});
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT: " + PORT);
+});
